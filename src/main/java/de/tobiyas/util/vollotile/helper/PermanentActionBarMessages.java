b73d244303a8ed5f2761add9b1437076740e2347
Tobias Welther <tobiyasw@gmail.com>
2016-01-17 21:44:13 +0100

    Add some extended Methods for the PermanentActionBar Task.

--- src/main/java/de/tobiyas/util/vollotile/helper/PermanentActionBarMessages.java
+++ src/main/java/de/tobiyas/util/vollotile/helper/PermanentActionBarMessages.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import de.tobiyas.util.schedule.DebugBukkitRunnable;
 import de.tobiyas.util.vollotile.VollotileCodeManager;
@@ -22,18 +23,20 @@ public class PermanentActionBarMessages {
 	 */
 	private final Map<UUID,String> messageMap = new HashMap<UUID,String>();
 	
-	
+	/**
+	 * the task running.
+	 */
+	private BukkitTask task;
 	
 	
 	protected PermanentActionBarMessages(Plugin plugin) {
-		new DebugBukkitRunnable("PerformanceActionbarMessager"){
+		this.task = new DebugBukkitRunnable("PerformanceActionbarMessager"){
 			@Override
 			protected void runIntern() {
 				Map<UUID,String> map = new HashMap<UUID, String>();
 				synchronized (messageMap) { map.putAll(messageMap); }
 				
 				Iterator<Map.Entry<UUID,String>> it = map.entrySet().iterator();
-				
 				while(it.hasNext()){
 					Map.Entry<UUID, String> entry = it.next();
 					UUID id = entry.getKey();
@@ -91,6 +94,24 @@ public class PermanentActionBarMessages {
 	public static PermanentActionBarMessages get(Plugin plugin){
 		if(instance == null) instance = new PermanentActionBarMessages(plugin);
 		return instance;
+	}
+	
+	/**
+	 * Returns true if the Message-Task is running.
+	 */
+	public static boolean isRunning(){
+		return instance != null;
+	}
+	
+	
+	/**
+	 * Finally kills the display!
+	 * <br>Call get again to recreate!
+	 */
+	public static void kill(){
+		if(instance == null) return;
+		instance.task.cancel();
+		instance = null;
 	}
 	
 }
