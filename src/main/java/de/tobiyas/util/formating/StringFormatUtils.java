b783dabcb3f9b6da955d53e903b0c7fe74daf13c
Tobias Welther <tobiyasw@gmail.com>
2016-03-19 01:22:41 +0100

    Add percent Formating

--- src/main/java/de/tobiyas/util/formating/StringFormatUtils.java
+++ src/main/java/de/tobiyas/util/formating/StringFormatUtils.java
@@ -1,5 +1,9 @@
 package de.tobiyas.util.formating;
 
+import org.bukkit.ChatColor;
+
+import de.tobiyas.util.math.Math2;
+
 public class StringFormatUtils {
 
 	
@@ -38,6 +42,45 @@ public class StringFormatUtils {
 		try{
 			return Double.parseDouble(val);
 		}catch(Throwable exp){ return defaultValue; }
+	}
+	
+	
+	/**
+	 * Formats the String to use.
+	 * 
+	 * @param percent to use
+	 * @param barLength to use
+	 * @param charToUse to use
+	 * @param fullColor to use
+	 * @param emptyColor to use.
+	 * 
+	 * @return the formatted String.
+	 */
+	public static String formatToPercent(double percent, int barLength, char charToUse, ChatColor fullColor, ChatColor emptyColor){
+		if(percent <= 0) percent = 0;
+		double presentBarLength = percent * barLength;
+		presentBarLength = Math2.clamp(0, presentBarLength, barLength);
+		
+		String barLeft = fullColor + "";
+		for(int i = 0; i < presentBarLength; i++){
+			barLeft += charToUse;
+			barLength --;
+		}
+		
+		String barRest = emptyColor + "";
+		for(; barLength > 0; barLength--){
+			barRest += charToUse;
+		}
+		
+		if(percent >= 1){
+			barRest += fullColor.toString() + charToUse;
+		}else{
+			barRest += emptyColor.toString() + charToUse;
+		}
+		
+		
+		String chatString = barLeft + barRest;
+		return chatString;
 	}
 	
 }
