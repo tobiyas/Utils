90a9da7b9ac39f3dd7e1f1ebd4fd3624ce110cc3
Tobias Welther <tobiyasw@gmail.com>
2016-04-15 15:25:26 +0200

    Add Some fancy Parse-Utils for Arrays.

--- src/main/java/de/tobiyas/util/formating/ParseUtils.java
+++ src/main/java/de/tobiyas/util/formating/ParseUtils.java
@@ -16,6 +16,17 @@ public class ParseUtils {
 	}
 	
 	/**
+	 * Parses an Integer from a String.
+	 * @param args to parse
+	 * @param index to use
+	 * @param defaultValue to use if failed.
+	 * @return the parsed value or default value.
+	 */
+	public static int parseInt(String[] args, int index, int defaultValue){
+		try{ return Integer.parseInt(args[index]); }catch(Throwable exp){ return defaultValue; }
+	}
+	
+	/**
 	 * Parses an Short from a String.
 	 * @param value to parse
 	 * @param defaultValue to use if failed.
@@ -23,6 +34,17 @@ public class ParseUtils {
 	 */
 	public static short parseShort(String value, short defaultValue){
 		try{ return Short.parseShort(value); }catch(Throwable exp){ return defaultValue; }
+	}
+	
+	/**
+	 * Parses an Short from a String.
+	 * @param args to parse
+	 * @param index to use
+	 * @param defaultValue to use if failed.
+	 * @return the parsed value or default value.
+	 */
+	public static short parseShort(String[] args, int index, short defaultValue){
+		try{ return Short.parseShort(args[index]); }catch(Throwable exp){ return defaultValue; }
 	}
 	
 	
@@ -35,6 +57,17 @@ public class ParseUtils {
 	public static double parseDouble(String value, double defaultValue){
 		try{ return Double.parseDouble(value); }catch(Throwable exp){ return defaultValue; }
 	}
+
+	/**
+	 * Parses an Double from a String.
+	 * @param args to parse
+	 * @param index to use
+	 * @param defaultValue to use if failed.
+	 * @return the parsed value or default value.
+	 */
+	public static double parseDouble(String[] args, int index, double defaultValue){
+		try{ return Double.parseDouble(args[index]); }catch(Throwable exp){ return defaultValue; }
+	}
 	
 	/**
 	 * Parses an Boolean from a String.
@@ -46,6 +79,17 @@ public class ParseUtils {
 		try{ return Boolean.parseBoolean(value); }catch(Throwable exp){ return defaultValue; }
 	}
 	
+	/**
+	 * Parses an Boolean from a String.
+	 * @param args to parse
+	 * @param index to use
+	 * @param defaultValue to use if failed.
+	 * @return the parsed value or default value.
+	 */
+	public static boolean parseBoolean(String[] args, int index, boolean defaultValue){
+		try{ return Boolean.parseBoolean(args[index]); }catch(Throwable exp){ return defaultValue; }
+	}
+	
 	
 	/**
 	 * Parses an Float from a String.
@@ -55,6 +99,17 @@ public class ParseUtils {
 	 */
 	public static float parseFloat(String value, float defaultValue){
 		try{ return Float.parseFloat(value); }catch(Throwable exp){ return defaultValue; }
+	}
+	
+	/**
+	 * Parses an Float from a String.
+	 * @param args to parse
+	 * @param index to use
+	 * @param defaultValue to use if failed.
+	 * @return the parsed value or default value.
+	 */
+	public static float parseFloat(String[] args, int index, float defaultValue){
+		try{ return Float.parseFloat(args[index]); }catch(Throwable exp){ return defaultValue; }
 	}
 	
 }
